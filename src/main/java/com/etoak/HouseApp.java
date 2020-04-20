2db3691a3c3086d94416680d044845841c5f9a27
shisan <1969299278@qq.com>
2020-04-20 11:46:45 +0800

    基本框架搭建完毕

--- src/main/java/com/etoak/HouseApp.java
+++ src/main/java/com/etoak/HouseApp.java
@@ -1,4 +1,10 @@
 package com.etoak;
 
+import org.springframework.boot.SpringApplication;
+
 public class HouseApp {
+     public static void main(String[] args) {
+             SpringApplication.run(HouseApp.class, args);
+
+      }
 }
