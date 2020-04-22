3eaebe2215cc69b3f6a83cb225d6d8ca2f381993
shisan <1969299278@qq.com>
2020-04-22 22:09:57 +0800

    最终添加页面  部分查询接口

--- src/main/java/com/etoak/utils/ValidateUtils.java
+++ src/main/java/com/etoak/utils/ValidateUtils.java
@@ -1,11 +1,7 @@
 package com.etoak.utils;
 
-<<<<<<< HEAD
 import com.etoak.exception.ParamException;
 import org.apache.commons.collections4.CollectionUtils;
-
-=======
->>>>>>> dev
 import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
 import javax.validation.Validator;
@@ -33,17 +29,5 @@ public class ValidateUtils {
             }
             throw new ParamException("参数错误 : " + buffer.toString());
         }
-=======
-    public static void validate(Object object){
-        Set<ConstraintViolation<Object>> violations=validator.validate(object);
-        Iterator<ConstraintViolation<Object>> iterator=violations.iterator();
-        StringBuffer buffer =new StringBuffer();
-        while(iterator.hasNext()){
-            ConstraintViolation<Object> violation=iterator.next();
-            String message=violation.getMessage();
-            buffer.append(message);
-        }
-        throw  new RuntimeException("参数错误： "+buffer.toString());
->>>>>>> dev
     }
 }
